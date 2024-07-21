d7b8dbee0c8994b231d1c67330e30335d530598e
ParkSuhhyun <2022111511@dgu.ac.kr>
2024-07-21 19:59:43 +0900

    feat: 장소 필터링

--- map/views.py
+++ map/views.py
@@ -32,15 +32,61 @@ def search_near_places(request):
             # category = random.choice(categories)
             while True:
                 category = random.choice(categories)
-                nearby_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=500&language=kr&type={category}&key={rest_api_key}"
+                nearby_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=1000&language=kr&type={category}&key={rest_api_key}"
                 nearby_response = requests.get(nearby_url).json()
-                if len(nearby_response['results']) != 0 :
-                    break 
+
+                # rating이 4.0 이상인 곳만 필터링
+                filtered_places = [place for place in nearby_response['results']
+                                    if place.get('rating', 0) >= 4.0
+                                    and not place.get('name', '').endswith(('점', '역', 'station)', '점)'))
+                                    and not place.get('name', '').startswith(("Paik's", 'Dunkin', '배스킨라빈스', 'KFC', 'Outback Steakhouse', "Domino's", 'Bonjuk', 'Burger King', '네네치킨', 'Pelicana Chicken', 'Bonjug', 'Pizza Maru'))
+                                    and '파리바게' not in place.get('name', '')
+                                    and 'Paris Baguette' not in place.get('name', '')
+                                    and '뚜레쥬르' not in place.get('name', '')
+                                    and 'Tous Les Jours' not in place.get('name', '')
+                                    and 'Starbucks' not in place.get('name', '')
+                                    and 'LOTTERIA' not in place.get('name', '')
+                                    and '설빙' not in place.get('name', '')
+                                    and 'Sulbing' not in place.get('name', '')
+                                    and 'Hollys' not in place.get('name', '')
+                                    and "Holly's" not in place.get('name', '')
+                                    and '인쌩맥주' not in place.get('name', '')
+                                    and '장미맨숀' not in place.get('name', '')
+                                    and 'Ediya' not in place.get('name', '')
+                                    and 'Pizza School' not in place.get('name', '')
+                                    and 'Daiso' not in place.get('name', '')
+                                    and 'KyoChon' not in place.get('name', '')
+                                    and 'Kyochon' not in place.get('name', '')
+                                    and "Mom's Touch" not in place.get('name', '')
+                                    and '스터디카페' not in place.get('name', '')
+                                    and '키즈카페' not in place.get('name', '')
+                                    and '7080' not in place.get('name', '')
+                                    and '상가' not in place.get('name', '')
+                                    and '프라자' not in place.get('name', '')
+                                    and 'Pizza Hut' not in place.get('name', '')
+                                    and 'Compose Coffee' not in place.get('name', '')
+                                    and 'Mega Coffee' not in place.get('name', '')
+                                    and 'Krispy Kreme' not in place.get('name', '')
+                                    and '로봇카페' not in place.get('name', '')
+                                    and '무인카페' not in place.get('name', '')
+                                    and '커스텀커피' not in place.get('name', '')
+                                    and 'Chicken Mania' not in place.get('name', '')
+                                    and 'BHC' not in place.get('name', '')
+                                    and 'BBQ' not in place.get('name', '')
+                                    and 'The Coffee Bean' not in place.get('name', '')
+                                    and '교촌' not in place.get('name', '')
+                                    and '피씨카페' not in place.get('name', '')
+                                    and 'Twosome Place' not in place.get('name', '')
+                                    and '샵' not in place.get('name', '')
+                                    and place.get('user_ratings_total', 0) >= 5
+                                    ]
+                if len(filtered_places) != 0:
+                    break
             
             result = {
                 'subway_station': location_response['candidates'][0],
                 'category' : category,
-                'nearby_places': nearby_response['results']
+                'nearby_places': filtered_places
             }
             return JsonResponse(result)
 
