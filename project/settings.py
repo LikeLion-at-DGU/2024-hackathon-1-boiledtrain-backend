a79cc9ec0c30d51a8c4444ad97e06cc0aaf45ca5
mingi <mintim01@naver.com>
2024-07-26 22:56:46 +0900

    fix: middle_ware -> cors 가장 높게 변경

--- project/settings.py
+++ project/settings.py
@@ -102,9 +102,10 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
