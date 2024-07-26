6f9f1e499cf3d7ae2042431729c1fe79ef9ce78e
mingi <mintim01@naver.com>
2024-07-26 17:14:41 +0900

    feat: kakao 세팅 sql 추가

--- user/urls.py
+++ user/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('course/<int:id>/', CourseViewSet.as_view({'get': 'retrieve'}), name='course-detail'),
     path('course/<int:course_id>/diary/', CourseDiaryViewSet.as_view({'get': 'retrieve', 'post': 'create', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='course-diary'),
     path('choose_and_add_place/', choose_and_add_place, name="choose_and_add_place"),
+    # 사진 출력
     path("search_photo" ,search_photo, name="search_photo"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
