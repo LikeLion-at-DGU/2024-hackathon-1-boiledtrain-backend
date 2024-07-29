2033dd0e70ab15e7640cba164ac1c79ca16b66aa
mingi <mintim01@naver.com>
2024-07-29 19:07:40 +0900

    feat: 프론트에서 jwt 토큰 보관 기능 추가

--- project/settings.py
+++ project/settings.py
@@ -212,6 +212,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    # 프론트 코드!!
+    'http://localhost:5173', 
+    'http://127.0.0.1:5173',
     'http://3.36.243.22',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
